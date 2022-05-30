74773a873103f87f5855e7d0ded5a31cb7c065aa
Alex Buck <alexjbuck@gmail.com>
2022-05-30 16:33:23 -0400

    Create pages for react router

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <>
+            <h1>
+                Task Force Ellyson
+            </h1>
+            <h4>
+                Build. Test. Deploy. Win.
+            </h4>
+        </>
+    )
+}
+    
