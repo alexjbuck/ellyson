d274ecf5520f8c6d7762c0ccd833826aec828912
Alex Buck <alexjbuck@gmail.com>
2022-06-01 08:38:01 -0400

    add simple search bar to projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import projects from './projects.json'
 import Project from '../Project'
 
 export default function Projects() {
   console.log(projects)
+
+  const [query, setQuery] = useState('');
+
   return (
     <>
       <h1>Projects</h1>
-      {projects.map(project => (
+      <input type="text" placeholder="Search" onChange={ e => setQuery(e.target.value) }/>
+      {projects
+      .filter(
+        project => Object.values(project).reduce( (prev,next) => prev || next.toString().toLowerCase().includes(query.toLowerCase()), false )
+      )
+      .map(project => (
         <Project key={project['Project ID']} project={project} />
       ))}
     </>
